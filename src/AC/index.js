b271caa5442bd144d36ea12b4f720c13d7123d79
Roman Iakobchuk <kaboom.ua@gmail.com>
2017-11-30 19:30:43 +0200

    add HW4

--- src/AC/index.js
+++ src/AC/index.js
@@ -1,13 +1,28 @@
-import {INCREMENT, DELETE_ARTICLE} from '../constants'
+import {INCREMENT, DELETE_ARTICLE, CHANGE_DATE_RANGE, CHANGE_SELECTION} from '../constants'
 
 export function increment() {
-    const action = { type: INCREMENT }
-    return action
+    return {
+        type: INCREMENT
+    }
 }
 
 export function deleteArticle(id) {
     return {
         type: DELETE_ARTICLE,
         payload: { id }
+    }
+}
+
+export function changeDateRange(dateRange) {
+    return {
+        type: CHANGE_DATE_RANGE,
+        payload: { dateRange }
+    }
+}
+
+export function changeSelection(selected) {
+    return {
+        type: CHANGE_SELECTION,
+        payload: { selected }
     }
 }
