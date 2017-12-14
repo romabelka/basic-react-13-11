2b8b0b6482bf3da710084ddc8b18582b0b8538e4
Roman Iakobchuk <kaboom.ua@gmail.com>
2017-12-14 15:40:04 +0200

    add HW7

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {Route, Switch, NavLink} from 'react-router-dom'
 import ArticlesPage from './routes/ArticlesPage'
+import CommentsPage from './routes/CommentsPage'
 import UserForm from './UserForm'
 import Filters from './Filters'
 import Counter from './Counter'
@@ -15,11 +16,13 @@ class App extends Component {
                     <div><NavLink to="/counter" activeStyle={{color: 'red'}}>counter</NavLink></div>
                     <div><NavLink to="/articles" activeStyle={{color: 'red'}}>articles</NavLink></div>
                     <div><NavLink to="/filters" activeStyle={{color: 'red'}}>filters</NavLink></div>
+                    <div><NavLink to="/comments/1" activeStyle = {{color: 'red'}}>comments</NavLink></div>
                 </div>
                 <Switch>
                     <Route path="/counter" component={Counter} strict exact/>
                     <Route path="/filters" component={Filters}/>
                     <Route path="/articles" component={ArticlesPage}/>
+                    <Route path = '/comments' component = {CommentsPage}/>
                 </Switch>
             </div>
         )
